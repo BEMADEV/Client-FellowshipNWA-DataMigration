b11c65402e975533f4abcc040f461ef4f9d8e4af
David Bensen <david.bensen@bemaservices.com>
2019-07-19 13:43:18 -0500

    Update person-attribute.sql
    Added HR Attributes, Added new category for those attributes

--- person-attribute.sql
+++ person-attribute.sql
@@ -43,3 +43,55 @@ WHERE TABLE_NAME = 'tblIndividual'
 		, 'BGCheckHoldReason'
 		, 'BackgroundCheckNote'
 	)
+
+UNION ALL
+
+SELECT
+	 'GB_' + [Column_Name] as [Key]
+	 ,[COLUMN_NAME] as [Name]
+	 , CASE Data_Type
+		WHEN 'datetime' THEN 'Rock.Field.Types.DateTimeFieldType'
+		WHEN 'smalldatetime' THEN 'Rock.Field.Types.DateTimeFieldType'
+		WHEN 'bit' THEN 'Rock.Field.Types.BooleanFieldType'
+		WHEN 'int' THEN 'Rock.Field.Types.IntegerFieldType'
+		ELSE 'Rock.Field.Types.TextFieldType' END as FieldType
+	, 'Grace Base HR' As Category
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE (TABLE_NAME = 'tblEmp'
+or TABLE_NAME = 'tblFamilyUsername'
+or TABLE_NAME = 'tblEmpPublicPosition'
+)
+	AND [Column_Name] In (
+	'SupervisorID'
+	, 'DirectLine'
+	, 'Intercom'
+	, 'MinistryArea'
+	, 'HoursPerDay'
+	, 'HoursPerWeek'
+	, 'DateOfHire'
+	, 'AnniversaryDate'
+	, 'ExemptStatus'
+	, 'TermDate'
+	, 'TermReason'
+	, 'TermSpcInstructions'
+	, 'RehireEligible'
+	, 'PayrollCode'
+	, 'PayrollName'
+	, 'PTOStartDate'
+	, 'SabbaticalStartDate'
+	, 'BenefitsStartDate'
+	, 'PrimaryPositionName'
+	, 'IDName'
+	, 'SabbaticalClass'
+	, 'PTFTPosition'
+	, 'SabbaticalLastTakenDate'
+	, 'SabbaticalNextEligibleDate'
+	, 'SabbaticalNotes'
+	, 'PositionType'
+	, 'WorkWeekDesc'
+	, 'MinistryArea'
+	, 'Title'
+	, 'Workphone'
+	, 'ShowOnGlobal'
+	, 'ShowOnCongregation'
+	)
