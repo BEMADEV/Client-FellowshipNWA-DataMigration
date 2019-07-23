461206b6a3e831939eb3bb9e5175912af135fa08
David Bensen <david.bensen@bemaservices.com>
2019-07-23 14:59:34 -0500

    Update financial-account.sql

--- financial-account.sql
+++ financial-account.sql
@@ -1,93 +1,145 @@
--- Use the Fund Type and Fund Group to Create a basic account hiaracy
 SELECT
-	-- ACTIVTY FUND
+	-- Benevolence Fund
 	990001 as Id
-	, 'Activity Fund' as [Name]
+	, 'Benevolence' as [Name]
 	, 'FALSE' as IsTaxDeductible
 	, '' As CampusId
 	, '' as ParentAccountId
-	, 1 as IsActive
-	, 1 as IsPublic
 
 UNION ALL
 
 SELECT
-	-- BUILDING FUND
+	-- Building Fund
 	990002 as Id
-	, 'Building Fund' as [Name]
+	, 'Building' as [Name]
 	, 'FALSE' as IsTaxDeductible
 	, '' As CampusId
 	, '' as ParentAccountId
-	, 1 as IsActive
-	, 1 as IsPublic
 
 UNION ALL
 
 SELECT
-	-- GENERAL FUND
+	-- Camp/Retreat Fund
 	990003 as Id
+	, 'Camp/Retreat' as [Name]
+	, 'FALSE' as IsTaxDeductible
+	, '' As CampusId
+	, '' as ParentAccountId
+
+UNION ALL
+
+SELECT
+	-- Childcare Fund
+	990004 as Id
+	, 'Childcare' as [Name]
+	, 'FALSE' as IsTaxDeductible
+	, '' As CampusId
+	, '' as ParentAccountId
+
+UNION ALL
+
+SELECT
+	-- Curriculum Fund
+	990005 as Id
+	, 'Curriculum' as [Name]
+	, 'FALSE' as IsTaxDeductible
+	, '' As CampusId
+	, '' as ParentAccountId
+
+UNION ALL
+
+SELECT
+	-- Elder Designated Fund
+	990006 as Id
+	, 'Elder Designated' as [Name]
+	, 'FALSE' as IsTaxDeductible
+	, '' As CampusId
+	, '' as ParentAccountId
+
+UNION ALL
+
+SELECT
+	-- General Fund Fund
+	990007 as Id
 	, 'General Fund' as [Name]
 	, 'FALSE' as IsTaxDeductible
 	, '' As CampusId
 	, '' as ParentAccountId
-	, 1 as IsActive
-	, 1 as IsPublic
 
 UNION ALL
 
 SELECT
-	-- Mission FUND
-	990004 as Id
-	, 'Mission Fund' as [Name]
+	-- Mission Fund
+	990008 as Id
+	, 'Mission' as [Name]
 	, 'FALSE' as IsTaxDeductible
 	, '' As CampusId
 	, '' as ParentAccountId
-	, 1 as IsActive
-	, 1 as IsPublic
 
 UNION ALL
 
 SELECT
-	-- Other FUND
-	990005 as Id
-	, 'Other Fund' as [Name]
+	-- Other Fund
+	990009 as Id
+	, 'Other' as [Name]
 	, 'FALSE' as IsTaxDeductible
 	, '' As CampusId
 	, '' as ParentAccountId
-	, 1 as IsActive
-	, 1 as IsPublic
 
 UNION ALL
 
 SELECT
-	-- NO FUND
-	990006 as Id
-	, 'No Fund' as [Name]
+	-- Passthrough Fund
+	990010 as Id
+	, 'Passthrough' as [Name]
 	, 'FALSE' as IsTaxDeductible
 	, '' As CampusId
 	, '' as ParentAccountId
-	, 1 as IsActive
-	, 1 as IsPublic
 
 UNION ALL
+
+SELECT
+	-- Service Fund
+	990011 as Id
+	, 'Service' as [Name]
+	, 'FALSE' as IsTaxDeductible
+	, '' As CampusId
+	, '' as ParentAccountId
+
+UNION ALL
+
+SELECT
+	-- Other Fund
+	990012 as Id
+	, 'Other' as [Name]
+	, 'FALSE' as IsTaxDeductible
+	, '' As CampusId
+	, '' as ParentAccountId
+
+UNION ALL
+
 
 -- Select all of the Designations
 SELECT
 	Id as Id
 	, Designations as [Name]
-	, CASE AChGroup
-		WHEN 'Giving' THEN 'TRUE'
-		ELSE 'FALSE'
+	, CASE DoNotReport
+		WHEN '1' THEN 'FALSE'
+		ELSE 'TRUE'
 	   END as IsTaxDeductible
 	, '' as CampusId
-	, CASE FundGroup
-		WHEN 'Activity Fund' THEN '990001'
-		WHEN 'Building Fund' THEN '990002'
-		WHEN 'General Fund' THEN '990003'
-		WHEN 'Mission Fund' THEN '990004'
-		WHEN 'Other Fund' THEN '990005'
-		ELSE '990006'
+	, CASE FundType
+		WHEN 'Benevolence' THEN '990001'
+		WHEN 'Building' THEN '990002'
+		WHEN 'Camp/Retreat' THEN '990003'
+		WHEN 'Childcare' THEN '990004'
+		WHEN 'Curriculum' THEN '990005'
+		WHEN 'Elder Designated' THEN '990006'
+		WHEN 'General Fund' THEN '990007'
+		WHEN 'Mission' THEN '990008'
+		WHEN 'Other' THEN '990009'
+		WHEN 'Passthrough' THEN '990010'
+		WHEN 'Service' THEN '990011'
+		ELSE '990012'
 		END as ParentAccountId
-	, IIF( inactive = 1, 0, 1 ) as IsActive
-	, IIF( DisplayOnWeb = 1, 1, 0 ) as IsPublic
 FROM lkDesignations
