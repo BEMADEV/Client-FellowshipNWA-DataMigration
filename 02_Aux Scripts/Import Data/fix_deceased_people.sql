7aa97ce919cc2b311456b2aee6a5b65ceeecabd3
Joshua Maiorino <33353876+JoshuaMaiorino@users.noreply.github.com>
2019-10-14 10:53:32 -0400

    Update fix_deceased_people.sql
    Updating the isDesceased column as well.

--- 02_Aux Scripts/Import Data/fix_deceased_people.sql
+++ 02_Aux Scripts/Import Data/fix_deceased_people.sql
@@ -1,6 +1,9 @@
 DECLARE @definedValueId int = (select v.Id from DefinedValue v where v.DefinedTypeId = 4 and v.Value like 'Deceased')
 
 update p
-set p.RecordStatusValueId = 4, p.RecordStatusReasonValueId = 167, p.InactiveReasonNote = 'Updated From SQL Import Job'
+set p.RecordStatusValueId = 4
+	, p.RecordStatusReasonValueId = 167
+	, p.InactiveReasonNote = 'Updated From SQL Import Job'
+	, p.IsDeceased = 1
 from Person p
 where p.ConnectionStatusValueId = @definedValueId
