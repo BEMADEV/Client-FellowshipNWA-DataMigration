1e1921b0a80d0719221b099c545d5b0da32ce619
Joshua Maiorino <33353876+JoshuaMaiorino@users.noreply.github.com>
2020-01-23 11:03:48 -0500

    Fix Person Import script to handle null values in nonind column.

--- 02_Aux Scripts/03_Update Scripts/Update Top Level Groups Parent Group Id to 520.sql
+++ 02_Aux Scripts/03_Update Scripts/Update Top Level Groups Parent Group Id to 520.sql
@@ -1,7 +1,7 @@
 Update g
 Set ParentGroupId = 520
 From [Group] g
-Where ForeignId is not null and GroupTypeId not in ( 10, 11)
+Where ForeignId is not null and GroupTypeId not in ( 10, 11, 32)
 and ParentGroupId is null
 and IsActive = 1
 and IsArchived = 0
