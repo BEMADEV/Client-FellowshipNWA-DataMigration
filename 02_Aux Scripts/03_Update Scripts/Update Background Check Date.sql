091c009cd10c87b080b8c40de0157e80b02a4052
Joshua Maiorino <33353876+JoshuaMaiorino@users.noreply.github.com>
2020-01-23 13:27:55 -0500

    Add Update to the scripts.

--- 02_Aux Scripts/03_Update Scripts/Update Background Check Date.sql
+++ 02_Aux Scripts/03_Update Scripts/Update Background Check Date.sql
@@ -1,12 +1,21 @@
 Declare @OldAttributeKey nvarchar(36) = 'GB_BackgroundCheckDate'
 		, @NewAttributeKey nvarchar(36) = 'BackgroundCheckDate'
 
---Insert Into AttributeValue (
---	IsSystem
---	, AttributeId
---	, EntityId
---	, [Value]
---	, [Guid] )
+Update av2
+	Set Value = av.[Value]
+From AttributeValue av
+Join Attribute a1 on a1.[Key] = @OldAttributeKey and a1.Id = av.AttributeId
+Join Attribute a2 on a2.[Key] = @NewAttributeKey
+Join AttributeValue av2 on av2.EntityId = av.EntityId and av2.AttributeId = a2.Id
+Where av2.Id is null
+and av.[Value] is not null or av.[Value] != ''
+
+Insert Into AttributeValue (
+	IsSystem
+	, AttributeId
+	, EntityId
+	, [Value]
+	, [Guid] )
 Select
 	IsSystem = 0
 	, AttributeId = a2.Id
