053cd5ebabb628ed65f32743f81d0368f8b37489
Joshua Maiorino <33353876+JoshuaMaiorino@users.noreply.github.com>
2020-02-19 13:56:14 -0500

    Import_Person_region

---
+++ 02_Aux Scripts/02_Import Data/Import_person_region.sql
@@ -0,0 +1,38 @@
+DECLARE @DefinedTypeId int = 112
+		, @DefaultValueId int = 1167
+		, @AttributeId int = 5958
+		, @ForeignKey varchar(11) = 'GB'
+; WITH CTE AS (
+	Select
+		p.Id as PersonId
+		, isNull(dv.Id, @DefaultValueId) as DefinedValueId
+	From 
+		_Tmp_Person_region2 pr
+	Join Person p on p.ForeignId = pr.PersonId and p.ForeignKey = @ForeignKey
+	Left Join DefinedValue dv on dv.Value = pr.FRegion and definedTypeId = 112
+)
+
+Select
+*
+Into #tmp
+From CTE
+
+--Select
+--	dv.[Guid]
+--From AttributeValue av
+--Join #tmp on #tmp.PersonId = av.EntityId and av.AttributeId = @AttributeId
+--Join DefinedValue dv on dv.Id = #tmp.DefinedValueId
+
+Insert Into AttributeValue( IsSystem, AttributeId, EntityId, [Value], [Guid] )
+Select 
+	0 as IsSystem
+	, @AttributeId as AttributeId
+	, #tmp.PersonId as EntityId
+	, Lower(dv.[Guid]) as [Value]
+	, NEWID() as [Guid]
+From #tmp
+Join DefinedValue dv on dv.Id = #tmp.DefinedValueId
+Left Join AttributeValue v on v.EntityId = #tmp.PersonId and v.AttributeId = @AttributeId
+Where v.id is null
+
+Drop Table #tmp
