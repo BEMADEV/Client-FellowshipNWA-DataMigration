464bc5604fd16b545e265a0f28c7cde32001835a
Joshua Maiorino <33353876+JoshuaMaiorino@users.noreply.github.com>
2020-04-21 09:29:38 -0400

    Update Update Legal Note.sql

--- 02_Aux Scripts/03_Update Scripts/Update Legal Note.sql
+++ 02_Aux Scripts/03_Update Scripts/Update Legal Note.sql
@@ -8,7 +8,7 @@ Join Attribute a1 on a1.[Key] = @OldAttributeKey and a1.Id = av.AttributeId
 Join Attribute a2 on a2.[Key] = @NewAttributeKey
 Join AttributeValue av2 on av2.EntityId = av.EntityId and av2.AttributeId = a2.Id
 Where av2.Id is null
-and av.[Value] is not null or av.[Value] != ''
+and ( av.[Value] is not null or av.[Value] != '' )
 
 Insert Into AttributeValue (
 	IsSystem
